9e4a1c21ef74024bf6182c2fb093d0830a084c85
mutturaj <mutturajdeval92@gmail.com>
2021-11-28 11:09:15 +0530

    dev commited

---
+++ src/main/java/com/javatechie/javatechie/JavatechieGithubDockerApplication.java
@@ -0,0 +1,13 @@
+package com.javatechie.javatechie;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JavatechieGithubDockerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JavatechieGithubDockerApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/javatechie/javatechie/JavatechieGithubDockerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.javatechie.javatechie;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JavatechieGithubDockerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
